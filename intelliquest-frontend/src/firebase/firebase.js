c3cdba263b4b00974bc5363fd8b6e5c5ae7dbe57
Zachary Polof <ZacharyPolof@gmail.com>
2024-12-01 10:48:08 -0600

    mvoed firebaseConfig to .env file and change id's for security

--- intelliquest-frontend/src/firebase/firebase.js
+++ intelliquest-frontend/src/firebase/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBudVdPBK0rN59kY82HCpNMluFe7n3BRF0",
-  authDomain: "intelliquest-login.firebaseapp.com",
-  projectId: "intelliquest-login",
-  storageBucket: "intelliquest-login.firebasestorage.app",
-  messagingSenderId: "959850301815",
-  appId: "1:959850301815:web:4f45fd35d9e65d2b4eb972",
-  measurementId: "G-NY4Z1BS406"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
